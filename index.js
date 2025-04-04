19837645b7936aee5d5bad0cfa828752dead0955
Mweresa Esther Magoma <esthermweresa27@gmail.com>
2025-04-04 09:45:02 +0300

    complete tests

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+return string.replace(/[^A-Za-z0-9-' ]+/g,"");
+  }
+
+
+  static titleize(inputString) {
+    const smallWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return inputString
+      .split(' ')
+      .map((word, index) => {
+        if (index === 0 || !smallWords.includes(word.toLowerCase())) {
+          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        } else {
+          return word.toLowerCase();
+        }
+      })
+      .join(' ');
+  }
 }
